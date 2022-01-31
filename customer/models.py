53c0993aae5c32c32cf49aa3223db4c340dd151a
mmdturan <mmdtnri@gmail.com>
2022-01-31 07:42:59 +0330

    models first try

--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Customer(User):
+    phone_number = models.CharField(max_length=12)
+    # phone_number = models.CharField(regex=r'^\+?1?\d{9,15}$')
+
+
+class Address(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    address = models.TextField(max_length=250)
+
+
+class Cart(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
--- food/models.py
+++ food/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from restaurant.models import MenuItem
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Food(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.ImageField()
+    menu_item = models.OneToOneField(MenuItem, null=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.TextField(max_length=250, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
