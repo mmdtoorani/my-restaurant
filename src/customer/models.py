0c6f09d44615c6f773506fb22a601086caac9b02
mmdturan <mmdtnri@gmail.com>
2022-08-24 00:08:09 +0430

    change structure

--- src/customer/models.py
+++ src/customer/models.py
@@ -6,11 +6,20 @@ class Customer(User):
     phone_number = models.CharField(max_length=12)
     # phone_number = models.CharField(regex=r'^\+?1?\d{9,15}$')
 
+    def __str__(self):
+        return self.username
+
 
 class Address(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     address = models.TextField(max_length=250)
 
+    def __str__(self):
+        return f"{self.customer.username}'s Address"
+
 
 class Cart(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.customer.username}'s Cart"
---
+++ src/customer/urls.py
@@ -0,0 +1,12 @@
+
+from django.urls import path
+
+#
+# urlpatterns = [
+#     path('login/', views.login, name='login'),
+#     path('signup/', views.signup, name='signup'),
+#     path('forget_password/', views.forget_password, name='forget_password'),
+#     path('profile/', views.profile, name='profile'),
+#     path('profile/edit', views.profile_edit, name='profile_edit'),
+#     path('logout/', views.auth_logout, name='logout'),
+# ]
